178618950554f8cec96a5317f82a85be6c9bad13
Robert Važan <robert.vazan@tutanota.com>
2021-03-22 13:13:26 +0000

    Implemented FingerprintImageOptions

---
+++ src/image.rs
@@ -0,0 +1,25 @@
+use core::panic;
+
+// Part of SourceAFIS for Rust: https://sourceafis.machinezoo.com/rust
+
+pub struct FingerprintImageOptions {
+    dpi: f64,
+}
+
+impl Default for FingerprintImageOptions {
+    fn default() -> Self {
+        FingerprintImageOptions {
+            dpi: 500.0,
+        }
+    }
+}
+
+impl FingerprintImageOptions {
+    pub fn dpi(&mut self, dpi: f64) -> &mut Self {
+        if dpi < 20.0 || dpi > 20_000.0 {
+            panic!("DPI is non-positive, impossibly low, or impossibly high.");
+        }
+        self.dpi = dpi;
+        self
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 // Part of SourceAFIS for Rust: https://sourceafis.machinezoo.com/rust
 
+mod image;
 mod number;
 mod point;
 
 pub(crate) use point::DoublePoint;
 pub(crate) use point::IntPoint;
 pub(crate) use point::Point;
+
+pub use image::FingerprintImageOptions;
 
 #[cfg(test)]
 mod test_utils {
